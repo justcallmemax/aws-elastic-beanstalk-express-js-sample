3e8973fdd6cb2b3361c504a958b02dfb4d8bc845
justcallmemax <maximiliano.m.vela@gmail.com>
2022-09-14 23:01:27 -0300

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Testing App, 1-2-3'));
+app.get('/', (req, res) => res.send('App is VERY different now'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
